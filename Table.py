e8cdc1957da3be3cdc3767060cc61c009655d16e
Vyshmas <vyshmasanthosh567@gmail.com>
2026-02-09 00:46:07 +0530

    all files

---
+++ Table.py
@@ -0,0 +1,5 @@
+print("Table of 5")
+print("--------------")
+for i in range(1,11):
+    table=i*5
+    print(i,"X 5 =",table)
---
+++ even_odd.py
@@ -0,0 +1,5 @@
+for i in range(1,21):
+    if i%2==0:
+        print("Even number is:",i)
+    else:
+        print("odd number is",i)
